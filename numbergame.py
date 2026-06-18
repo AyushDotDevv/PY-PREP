a68726e8d5d648d3115f84e3e4540b402d6f242e
Ayush Patel <reachap02@gmail.com>
2026-06-19 03:06:35 +0530

    Project 1: added play again feature

--- numbergame.py
+++ numbergame.py
@@ -1,22 +1,29 @@
-#guessingGame 
-import random 
-a=random.randint(1,100)
-print (a)
-x=0
-while True :
-    b=int(input ("enter your guess"))
-    if x<5:
-        if b<a:
+#guessingGame
+def gg ():
+    import random 
+    a=random.randint(1,100)
+    print (a)
+    x=0
+    while True :
+      b=int(input ("enter your guess"))
+      if x<5:
+         if b<a:
             print ("too low , try again !!")
             x=x+1
-        elif b>a:
+         elif b>a:
             print("too high , try again !!")
             x=x+1
-        else :
+         else :
             print ("you gussed it right")
             x=x+1
             break
-    else:
-        print ("out of attempts")  
-        break  
-print(f"number of attempts are {x}")
+      else:
+           print ("out of attempts")  
+           break 
+gg() 
+x=input("wanna play again?? y/n")
+while True :
+   if x=="y":
+      gg()
+   else :
+      print ("over")        
